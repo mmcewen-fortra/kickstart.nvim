ed6c435a0aabc3a3b02d2cae443bf96b0c540d8e
Mason McEwen <mason.mcewen@fortra.com>
2024-11-11 09:16:10 -0700

    Update lint tools

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -5,10 +5,14 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       local lint = require 'lint'
-      lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
-      }
+      lint.linters_by_ft = lint.linters_by_ft or {}
+      -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
+      lint.linters_by_ft['javascript'] = { 'oxlint' }
+      lint.linters_by_ft['typescript'] = { 'oxlint' }
+      lint.linters_by_ft['javascriptreact'] = { 'oxlint' }
+      lint.linters_by_ft['typescriptreact'] = { 'oxlint' }
 
+      -- markdown = { 'markdownlint' },
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
       -- instead set linters_by_ft like this:
       -- lint.linters_by_ft = lint.linters_by_ft or {}
