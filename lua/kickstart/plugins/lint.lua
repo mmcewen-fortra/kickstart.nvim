8e3f544ebf682d11fed6a566911aed6d0033c3ff
Mason McEwen <mason.mcewen@fortra.com>
2024-11-13 08:41:03 -0700

    Update linter to use both oxlint and eslint

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,10 +7,10 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = lint.linters_by_ft or {}
       -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
-      lint.linters_by_ft['javascript'] = { 'oxlint' }
-      lint.linters_by_ft['typescript'] = { 'oxlint' }
-      lint.linters_by_ft['javascriptreact'] = { 'oxlint' }
-      lint.linters_by_ft['typescriptreact'] = { 'oxlint' }
+      lint.linters_by_ft['javascript'] = { 'oxlint', 'eslint_d' }
+      lint.linters_by_ft['typescript'] = { 'oxlint', 'eslint_d' }
+      lint.linters_by_ft['javascriptreact'] = { 'oxlint', 'eslint_d' }
+      lint.linters_by_ft['typescriptreact'] = { 'oxlint', 'eslint_d' }
 
       -- markdown = { 'markdownlint' },
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
