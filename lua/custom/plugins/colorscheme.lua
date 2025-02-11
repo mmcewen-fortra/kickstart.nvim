c3067d49816193667c4fcd1ba57e97bb294ccc4b
Mason McEwen <mason.mcewen@fortra.com>
2025-02-11 09:25:13 -0700

    Add mdx support

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,24 +1,26 @@
-return {
-  'talha-akram/noctis.nvim',
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd 'colorscheme noctis'
-  end,
-}
--- cyberdream theme (great for transparent editor)
+-- noctis theme
 -- return {
---   'scottmckendry/cyberdream.nvim',
+--   'talha-akram/noctis.nvim',
 --   lazy = false,
 --   priority = 1000,
 --   config = function()
---     require('cyberdream').setup {
---       transparent = true,
---       terminal_colors = false,
---     }
---     vim.cmd 'colorscheme cyberdream'
+--     vim.cmd 'colorscheme noctis'
 --   end,
 -- }
+
+-- cyberdream theme (great for transparent editor)
+return {
+  'scottmckendry/cyberdream.nvim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require('cyberdream').setup {
+      transparent = true,
+      terminal_colors = false,
+    }
+    vim.cmd 'colorscheme cyberdream'
+  end,
+}
 
 -- josean's theme
 -- return {
